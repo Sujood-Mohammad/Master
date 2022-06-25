fef818e9f2717e769a659e28fb916812609c19bd
Sujood-Mohammad <sujoodmoh1996@gmail.com>
2022-06-26 01:31:52 +0300

    main

---
+++ sujoud-master/routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', function () {
+    return view('Pages/index');
+});
+Route::get('/about', function () {
+    return view('Pages.about');
+});
+Route::get('/shop', function () {
+    return view('Pages.shop');
+});
+Route::get('/contact', function () {
+    return view('Pages.contact');
+});
+Route::get('/cart', function () {
+    return view('Pages.cart');
+});
+Route::get('/product', function () {
+    return view('Pages.product-details');
+});
+
+
+
+
+
